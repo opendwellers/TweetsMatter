e1dcd7e49be6d6aa78a13b7844b2327052667fe2
root <root@localhost.localdomain>
2016-08-03 22:25:36 -0400

    streaming trump tweets on mattermost sandbox hook

--- App.py
+++ App.py
@@ -3,6 +3,7 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import simplejson as json
 import configparser as ConfigParser
+import urllib3
 
 Config = ConfigParser.ConfigParser()
 Config.read("conf/config")
@@ -11,11 +12,18 @@ c_key = Config.get('twitter','Ckey')
 c_secret = Config.get('twitter','CSecret')
 a_token = Config.get('twitter','AToken')
 a_secret = Config.get('twitter','ASecret')
+hook = Config.get('mattermost','Hook')
+avatar = 'http://i.imgur.com/RCKRF2B.png'
+
+http = urllib3.PoolManager()
 
 class listener(StreamListener):
     def on_data(self, data):
         data = json.loads(data)
-        print(data.get("text")) 
+        print(data.get("text"))
+        payload = {'username' : 'Trump', 'icon_url' : avatar, 'text' : data.get("text")}
+        r = http.request('POST',hook ,headers={'Content-Type':'application/json'} ,body=json.dumps(payload))
+        print(r.read())
         return True
 
     def on_error(self, status):
@@ -24,4 +32,4 @@ class listener(StreamListener):
 auth = OAuthHandler(c_key, c_secret)
 auth.set_access_token(a_token, a_secret)
 twitterStream = Stream(auth, listener())
-twitterStream.filter(follow=['760836832699359233'])
+twitterStream.filter(follow=['25073877'])
