00df221b8812078ceb1ff6a2b7de4c8bf202fe38
root <root@localhost.localdomain>
2016-08-03 20:57:45 -0400

    listening to user bagelxd (hardcoded twitter id)

--- App.py
+++ App.py
@@ -8,8 +8,6 @@ c_secret = "RbGIUEZhdHwId1GB01K8T0fTPnwah0Pc0q3RcxKIETq8dMnGfh"
 a_token = "760836832699359233-oJv1BuImm2HqR5zHfA9klh415lMG3sq"
 a_secret = "Llh4zbFuUj6fNfPb3QsuFFSr9oC0Bq51z0paKTndIehlM"
 
-print("Authenticated with cosumer key : "+c_key+" and consumer secret :  "+c_secret)
-
 class listener(StreamListener):
     def on_data(self, data):
         data = json.loads(data)
@@ -22,4 +20,4 @@ class listener(StreamListener):
 auth = OAuthHandler(c_key, c_secret)
 auth.set_access_token(a_token, a_secret)
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["car"])
+twitterStream.filter(follow=['760836832699359233'])
