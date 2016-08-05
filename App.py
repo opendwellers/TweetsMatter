931c7a28f47454b7e1e394052ef867f714070092
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2016-08-05 10:28:08 -0400

    Set the configuration file into the program's argument

--- App.py
+++ App.py
@@ -1,6 +1,8 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import os
+import sys
 import simplejson as json
 import configparser as ConfigParser
 import urllib3
@@ -25,20 +27,40 @@ class listener(StreamListener):
 
 
 if __name__ == '__main__':
+    # Make sure the config file was given
+    if (len(sys.argv) != 2):
+        print("You must only specify the path to the configuration file.")
+        exit(1)
+
+    # Make sure the file path is valid
+    path = sys.argv[1]
+    if (not os.path.isfile(path)):
+        print("The specified config file `" + path + "` doesn't exist.")
+        exit(1)
+
+    # Load the configuration file
     Config = ConfigParser.ConfigParser()
-    Config.read("conf/config")
+    Config.read(path)
 
-    c_key = Config.get('twitter','Ckey')
-    c_secret = Config.get('twitter','CSecret')
-    a_token = Config.get('twitter','AToken')
-    a_secret = Config.get('twitter','ASecret')
-    avatar = Config.get('mattermost', "AvatarURL")
-    hook = Config.get('mattermost','Hook')
+    # Parse the configuration
+    try:
+        c_key = Config.get('twitter','Ckey')
+        c_secret = Config.get('twitter','CSecret')
+        a_token = Config.get('twitter','AToken')
+        a_secret = Config.get('twitter','ASecret')
+        avatar = Config.get('mattermost', "AvatarURL")
+        hook = Config.get('mattermost','Hook')
+    except ConfigParser.NoOptionError as exception:
+        print(exception)
+        exit(1)
 
+    # Get an http manager
     http = urllib3.PoolManager()
 
-
+    # Create Twitter OAuth tokens
     auth = OAuthHandler(c_key, c_secret)
     auth.set_access_token(a_token, a_secret)
+
+    # Create Twitter stream
     twitterStream = Stream(auth, listener())
     twitterStream.filter(follow=['25073877'])
